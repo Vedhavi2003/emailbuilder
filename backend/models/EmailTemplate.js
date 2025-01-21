1ac85d7f01bc5e7339e20bc142dd4b673a96db84
Vedhavi2003 <vedhavimarepally@gmail.com>
2025-01-21 12:45:08 +0530

    Update EmailTemplate.js

--- backend/models/EmailTemplate.js
+++ backend/models/EmailTemplate.js
@@ -1,1 +1,12 @@
+const mongoose = require('mongoose');
+
+const EmailTemplateSchema = new mongoose.Schema({
+  title: String,
+  content: String,
+  footer: String,
+  imageUris: [String],
+  createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('EmailTemplate', EmailTemplateSchema);
 
