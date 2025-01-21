88537dea6f666265fbf19c6e1a75cb45357f4c32
Vedhavi2003 <vedhavimarepally@gmail.com>
2025-01-21 12:46:18 +0530

    Update ImageUploader.js

--- frontend/src/components/ImageUploader.js
+++ frontend/src/components/ImageUploader.js
@@ -1,1 +1,18 @@
+import React from 'react';
+import axios from 'axios';
+
+const ImageUploader = ({ onUpload }) => {
+  const handleImageUpload = async (e) => {
+    const file = e.target.files[0];
+    const formData = new FormData();
+    formData.append('image', file);
+
+    const response = await axios.post('http://localhost:5000/api/uploads', formData);
+    onUpload(response.data.imageUrl);
+  };
+
+  return <input type="file" onChange={handleImageUpload} />;
+};
+
+export default ImageUploader;
 
