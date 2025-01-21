3990d23d878c07d64cf8cab801fe7d6a62616932
Vedhavi2003 <vedhavimarepally@gmail.com>
2025-01-21 12:44:36 +0530

    Update uploadroutes.js

--- backend/routes/uploadroutes.js
+++ backend/routes/uploadroutes.js
@@ -1,1 +1,18 @@
+const express = require('express');
+const multer = require('multer');
+const router = express.Router();
+
+const storage = multer.diskStorage({
+  destination: './uploads/',
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`);
+  },
+});
+const upload = multer({ storage });
+
+router.post('/', upload.single('image'), (req, res) => {
+  res.json({ imageUrl: `/uploads/${req.file.filename}` });
+});
+
+module.exports = router;
 
