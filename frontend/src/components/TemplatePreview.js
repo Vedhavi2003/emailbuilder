b9234386ad23ab0a0c49d25514027eb867e4497d
Vedhavi2003 <vedhavimarepally@gmail.com>
2025-01-21 12:46:51 +0530

    Update TemplatePreview.js

--- frontend/src/components/TemplatePreview.js
+++ frontend/src/components/TemplatePreview.js
@@ -1,1 +1,17 @@
+import React from 'react';
+
+const TemplatePreview = ({ template }) => {
+  return (
+    <div className="template-preview">
+      <h1>{template.title}</h1>
+      <p>{template.content}</p>
+      <footer>{template.footer}</footer>
+      {template.images.map((url, index) => (
+        <img key={index} src={url} alt="Uploaded" />
+      ))}
+    </div>
+  );
+};
+
+export default TemplatePreview;
 
